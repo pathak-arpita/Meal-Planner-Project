5ad854f7ccd67ddb32e7bb47f5cd456b056c215f
Arpita <pathak.arpita6497@gmail.com>
2023-07-14 22:39:56 +0530

    initial commit

--- App.js
+++ App.js
@@ -17,21 +17,31 @@ const tbody = document.querySelector("tbody")
 const load = document.querySelector("#load");
 const background = document.querySelector(".background");
 const recipebackground = document.querySelector(".recipe");
- const formData = document.querySelector(".formData");
+const formData = document.querySelector(".formData");
+let toast = document.createElement('div');
 
 // api Key 
 const apiKey1 =  "6d9547fa048e42759cdedfee1d0bedda";
-const apiKey2 = "a02399d93f914654a7bec3bc12eba59f";
+const apiKey6 = "a02399d93f914654a7bec3bc12eba59f";
 const apiKey = "4c30305842244bdc959fe01d7ba4daf1";
 const apiKey3 = "2530ebb758e9458b97b8ea62b2f8a259";
 const apiKey5 = "58b70ad75cfd44faaf2caaee62677046";
-const apiKey6 = "7a4a6efeb7d34eeda9b5152e3ddcce57";
+// const apiKey6 = "7a4a6efeb7d34eeda9b5152e3ddcce57";
 
 
 async function mealColories() {
     if (age.value >100){
-        toastr.success('This is a notification!', 'Custom Alert');
+        toast.classList.add('toast');
+        toast.innerHTML = '<i class="fa-solid fa-circle-xmark"> </i>Please Ente valid Inputs';
+        toastBox.appendChild(toast);
+
+        setTimeout(()=>{
+           toast.remove();
+        } , 5000)
+
+        blankData();
     }
+
     let bmrMale = 66.47 + (13.75 * weight.value) + (5.003 * height.value) - (6.755 * age.value);
     let bmrFemale = 655.1 + (9.563 * weight.value) + (1.850 * height.value) - (4.676 * age.value);
     if (gender.value === "male" && activity.value === "light") {
